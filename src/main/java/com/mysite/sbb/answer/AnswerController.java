6d2af926b7bc1ed96a8ce703d965cad33a527a6b
Snu97 <snu970814@naver.com>
2022-09-21 15:54:30 +0900

    Changes

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -18,6 +18,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 @RequestMapping("/answer")
 @RequiredArgsConstructor
@@ -40,5 +43,30 @@ public class AnswerController {
 		}
 		this.answerService.create(question, answerForm.getContent(), siteUser);
 		return String.format("redirect:/question/detail/%s", id);
+	}
+
+	@PreAuthorize("isAuthenticated()")
+	@GetMapping("/modify/{id}")
+	public String answerModify(AnswerForm answerForm, @PathVariable("id") Integer id, Principal principal){
+		Answer answer = this.answerService.getAnswer(id);
+		if(!answer.getAuthor().getUsername().equals(principal.getName())){
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정 권한이 없습니다.");
+		}
+		answerForm.setContent(answer.getContent());
+		return "answer_form";
+	}
+
+	@PreAuthorize("isAuthenticated()")
+	@PostMapping("/modify/{id}")
+	public String answerModify(@Valid AnswerForm answerForm, BindingResult bindingResult, @PathVariable("id") Integer id, Principal principal){
+		if(bindingResult.hasErrors())
+			return "answer_form";
+
+		Answer answer = this.answerService.getAnswer(id);
+		if(!answer.getAuthor().getUsername().equals(principal.getName())){
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+		}
+		this.answerService.modify(answer, answerForm.getContent());
+		return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
 	}
 }
